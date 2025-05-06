cef1a030353a7bbc4f9906fc7ff575df8285ff1e
ja3cob <jakubecker@gmail.com>
2025-05-06 23:42:04 +0200

    Add logout button, redirect to login when fetch failed

--- todo-frontend/src/components/Dashboard.jsx
+++ todo-frontend/src/components/Dashboard.jsx
@@ -1,19 +1,31 @@
 import React, { useState, useEffect } from "react";
 import api from "../util/api";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState("");
   const [newTodoDate, setNewTodoDate] = useState(new Date().toISOString().split('T')[0]);
   const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split('T')[0]);
+  const navigate = useNavigate();
 
   const fetchTodos = async () => {
     const token = localStorage.getItem("token");
-    const response = await api.get("todos", {
-      headers: { Authorization: `Bearer ${token}` },
-      params: { date: selectedDate }
-    });
-    setTodos(response.data);
+    try {
+      if(!token) {
+        throw Error;
+      }
+      
+      const response = await api.get("todos", {
+        headers: { Authorization: `Bearer ${token}` },
+        params: { date: selectedDate }
+      });
+
+      setTodos(response.data);
+    }
+    catch {
+      navigate("/");
+    }
   };
 
   useEffect(() => {
@@ -53,7 +65,17 @@ const Dashboard = () => {
 
   return (
     <div className="p-8 max-w-2xl mx-auto bg-white shadow-md rounded-xl">
-      <h2 className="text-3xl font-bold mb-6">Super To-Do App</h2>
+      <div className="flex items-center justify-between mb-6">
+        <h2 className="text-3xl font-bold">Super To-Do App</h2>
+        <button
+          onClick={() => {
+            localStorage.removeItem("token");
+            window.location.href = "/";
+          }}
+          className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">
+          Logout
+        </button>
+      </div>
       <div className="mb-6 flex flex-col sm:flex-row gap-2">
         <input
           type="text"
